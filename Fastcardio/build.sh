25bc61ce12a6df3434660f3710797b563890fb6a
Ephraim19 <machayophraim@gmail.com>
2024-12-28 00:31:56 +0300

    Requirements

--- Fastcardio/build.sh
+++ Fastcardio/build.sh
@@ -6,13 +6,10 @@ set -o errexit
 pip install -r requirements.txt
 
 # Convert static asset files
+
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
+python manage.py makemigrations
 python manage.py migrate
 
-
-if [[ $CREATE_SUPERUSER ]];
-then
-  python manage.py createsuperuser --no-input
-fi
