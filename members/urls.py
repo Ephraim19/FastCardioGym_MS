d19fd368b91c129027766112207fdb4631b54bfd
Ephraim19 <machayophraim@gmail.com>
2025-01-19 18:45:28 +0300

    fix: urls revamp

--- members/urls.py
+++ members/urls.py
@@ -22,10 +22,6 @@ urlpatterns = [
     path('member-details/<int:member_id>/', views.get_member_details, name='get_member_details'),
     path('check-in-out/', views.check_in_out, name='check_in_out'),
     
-    # path('checkin/',views.checkin,name="Checkin" ),
-    # path('checkin/check-in-out/', views.check_in_out, name='check_in_out'),
-    # path('checkin/member-history/', views.get_member_history, name='member_history'),
-    
     path('finance/',views.finance,name="Finance" ),
     path('revenue-summary/', views.RevenueAndMembershipView.as_view(), name='revenue-summary'),
     
